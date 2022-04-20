16e802aabc964f777ba2f02bf48b7f82e609d12e
dannymaclaughlin <dannymaclaughlin@gmail.com>
2022-04-19 19:34:56 -0700

    added index.html and index.js files

---
+++ index.js
@@ -0,0 +1,16 @@
+const words = ['rock', 'paper', 'scissors'];
+const word = words[Math.floor(Math.random()*words.length)];
+
+function computerPlay() {
+    return word;
+} 
+
+//create a function called computerPlay                           
+//function should return: rock, paper, or scissors
+function playRound(playerSelection, computerSelection) {
+    
+} //create new function to play a single round of rock paper scissors: playGame
+//playRound should take two parameters - playerSelection and computerSelection
+//return string that declares winner of the round like "you lose! paper beats rock"
+//make playerSelection parameter case-insensitive(so users can input rock, ROCK, or RocK)
+//return results of this function call, do not console.log() them
