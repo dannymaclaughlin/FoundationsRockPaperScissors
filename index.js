2e9cb47dc747d03d877fc8dbfca073650223f922
dannymaclaughlin <dannymaclaughlin@gmail.com>
2022-04-21 23:35:24 -0700

    added playRound functions + comp choses rps at random

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const choices = ['rock', 'paper', 'scissors'];
 
+//create a function called computerPlay
+
+//function should return: rock, paper, or scissors. choices variable should live outside of the function in the main
+//scope so that it can be called outside of the function
 function computerPlay() {
     const choice = choices[Math.floor(Math.random() * (choices.length))];
     return choice;
 } 
-
-//create a function called computerPlay                           
-//function should return: rock, paper, or scissors
+                         
 function playRound(playerSelection, computerSelection) {
-    
-} //create new function to play a single round of rock paper scissors: playGame
+    if (computerSelection() === 'rock') {
+    return 'You Loser! Rock beats Scissors';
+    } else if (computerSelection() === 'paper') {
+    return 'You Loser! Paper beats Rock';
+    } else if (computerSelection() === 'scissors') {
+    return 'You Loser! Scissors beats paper'
+    }
+} 
+
+const playerSelection = "rock";
+function computerSelection() {
+    if computerSelection === computerPlay();
+}
+//create new function to play a single round of rock paper scissors: playRound
 //playRound should take two parameters - playerSelection and computerSelection
 //return string that declares winner of the round like "you lose! paper beats rock"
 //make playerSelection parameter case-insensitive(so users can input rock, ROCK, or RocK)
