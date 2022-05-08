d889929511214a349b8b961499a70ec69be00dea
dannymaclaughlin <dannymaclaughlin@gmail.com>
2022-05-07 23:58:31 -0700

    fixed some =(assignment) that should've been ===(comparison). code will run one round successfully and return a win, tie, or loss.

--- index.js
+++ index.js
@@ -8,30 +8,24 @@ let playerSelection = prompt('rock, paper, or scissors?');
 let computerSelection = computerPlay();
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection;
+    playerSelection = playerSelection.toLowerCase();
     computerSelection = computerPlay();
 
     if (playerSelection === computerSelection) {
         return 'you tied';
-    } else if (computerSelection = 'paper' && playerSelection === 'rock') {
-        return 'you lose, paper beats rock!';
-    } else if (computerSelection = 'scissors' && playerSelection === 'paper') {
-        return 'you lose, scissors beats paper!';
-    } else if (computerSelection = 'rock' && playerSelection === 'scissors') {
-        return 'you lose, rock beats scissors!';
-    } else if (computerSelection = 'rock' && playerSelection === 'paper') {
-        return 'you win, paper beats rock!';
-    } else if (computerSelection = 'paper' && playerSelection === 'scissors') {
-        return 'you win, scissors beats paper!';
-    } else if (computerSelection = 'scissors' && playerSelection === 'rock') {
-        return 'you win, rock beats scissors!';
+    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+        return 'you lose, paper beats rock! ):';
+    } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+        return 'you lose, scissors beats paper! ):';
+    } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
+        return 'you lose, rock beats scissors! ):';
+    } else if (computerSelection === 'rock' && playerSelection === 'paper') {
+        return 'you win, paper beats rock! (:';
+    } else if (computerSelection === 'paper' && playerSelection === 'scissors') {
+        return 'you win, scissors beats paper! (:';
+    } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
+        return 'you win, rock beats scissors! (:';
     }
 }
 
 console.log(playRound(playerSelection, computerSelection));
-
-//create new function to play a single round of rock paper scissors: playRound
-//playRound should take two parameters - playerSelection and computerSelection
-//return string that declares winner of the round like "you lose! paper beats rock"
-//make playerSelection parameter case-insensitive(so users can input rock, ROCK, or RocK)
-//return results of this function call, do not console.log() them
